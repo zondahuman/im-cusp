169276ad029b065837021bf89072c8ff19699af9
zondahuman <zondahuman@gmail.com>
2018-06-14 18:00:20 +0800

    add

---
+++ im-ccommon/src/test/java/com/abin/lee/im/custom/common/test/AppTest.java
@@ -0,0 +1,7 @@
+package com.abin.lee.im.custom.common.test;
+
+/**
+ * Created by abin on 2018/6/14.
+ */
+public class AppTest {
+}
---
+++ im-cserver/src/test/java/com/abin/lee/im/cserver/test/AppTest.java
@@ -0,0 +1,7 @@
+package com.abin.lee.im.cserver.test;
+
+/**
+ * Created by abin on 2018/6/14.
+ */
+public class AppTest {
+}
